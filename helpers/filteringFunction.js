db6f993ea5ca198bfc215b4212837956be50ce4c
alvar0torres <995.alvaro@gmail.com>
2024-04-01 19:33:19 +0200

    improve filter results helper function

--- helpers/filteringFunction.js
+++ helpers/filteringFunction.js
@@ -1,195 +1,66 @@
 import calculateRemainingTime from "./remainingTimeCalculator";
 
 function filteringFunction(status, priceRange, array) {
-  let filteredArray = [];
 
-  if (status === "" && priceRange === "") {
-    filteredArray = array;
-  } else if (status === "" && priceRange === "upTo5k") {
-    filteredArray = array.filter(
-      (auction) => parseFloat(auction.price.replace(/,/g, "")) <= 5000
+  function isActive(auction) {
+    return calculateRemainingTime(auction.expirationTime) > 0;
+  }
+
+  function isSold(auction) {
+    return (
+      calculateRemainingTime(auction.expirationTime) < 0 &&
+      auction.lastBidder !== ""
     );
-  } else if (status === "" && priceRange === "upTo10k") {
-    filteredArray = array.filter(
-      (auction) =>
-        parseFloat(auction.price.replace(/,/g, "")) <= 10000 &&
-        parseFloat(auction.price.replace(/,/g, "")) >= 5000
+  }
+
+  function isClosed(auction) {
+    return (
+      calculateRemainingTime(auction.expirationTime) < 0 &&
+      auction.lastBidder === ""
     );
-  } else if (status === "" && priceRange === "upTo20k") {
-    filteredArray = array.filter(
-      (auction) =>
-        parseFloat(auction.price.replace(/,/g, "")) <= 20000 &&
-        parseFloat(auction.price.replace(/,/g, "")) >= 10000
-    );
-  } else if (status === "" && priceRange === "upTo50k") {
-    filteredArray = array.filter(
-      (auction) =>
-        parseFloat(auction.price.replace(/,/g, "")) <= 50000 &&
-        parseFloat(auction.price.replace(/,/g, "")) >= 20000
-    );
-  } else if (status === "" && priceRange === "upTo100k") {
-    filteredArray = array.filter(
-      (auction) =>
-        parseFloat(auction.price.replace(/,/g, "")) <= 100000 &&
-        parseFloat(auction.price.replace(/,/g, "")) >= 50000
-    );
-  } else if (status === "" && priceRange === "moreThan100k") {
-    filteredArray = array.filter(
-      (auction) => parseFloat(auction.price.replace(/,/g, "")) >= 100000
-    );
-  } else if (status === "Active" && priceRange === "") {
-    filteredArray = array.filter(
-      (auction) => calculateRemainingTime(auction.expirationTime) > 0
-    );
-  } else if (status === "Active" && priceRange === "upTo5k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter((auction) => parseFloat(auction.price.replace(/,/g, "")) <= 5000);
-  } else if (status === "Active" && priceRange === "upTo10k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 10000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 5000
-      );
-  } else if (status === "Active" && priceRange === "upTo20k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 20000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 10000
-      );
-  } else if (status === "Active" && priceRange === "upTo50k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 50000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 20000
-      );
-  } else if (status === "Active" && priceRange === "upTo100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 100000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 50000
-      );
-  } else if (status === "Active" && priceRange === "moreThan100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) > 0)
-      .filter(
-        (auction) => parseFloat(auction.price.replace(/,/g, "")) >= 100000
-      );
-  } else if (status === "Sold" && priceRange === "") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "upTo5k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter((auction) => parseFloat(auction.price.replace(/,/g, "")) <= 5000)
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "upTo10k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 10000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 5000
-      )
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "upTo20k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 20000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 10000
-      )
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "upTo50k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 50000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 20000
-      )
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "upTo100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 100000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 50000
-      )
-      .filter((auction) => auction.lastBidder != "");
-  } else if (status === "Sold" && priceRange === "moreThan100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) => parseFloat(auction.price.replace(/,/g, "")) >= 100000
-      )
-      .filter((auction) => auction.lastBidder != "");
-  }  else if (status === "Closed" && priceRange === "") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "upTo5k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter((auction) => parseFloat(auction.price.replace(/,/g, "")) <= 5000)
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "upTo10k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 10000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 5000
-      )
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "upTo20k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 20000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 10000
-      )
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "upTo50k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 50000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 20000
-      )
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "upTo100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) =>
-          parseFloat(auction.price.replace(/,/g, "")) <= 100000 &&
-          parseFloat(auction.price.replace(/,/g, "")) >= 50000
-      )
-      .filter((auction) => auction.lastBidder === "");
-  } else if (status === "Closed" && priceRange === "moreThan100k") {
-    filteredArray = array
-      .filter((auction) => calculateRemainingTime(auction.expirationTime) < 0)
-      .filter(
-        (auction) => parseFloat(auction.price.replace(/,/g, "")) >= 100000
-      )
-      .filter((auction) => auction.lastBidder === "");
+  }
+
+  function isPriceInRange(auction) {
+    const price = parseFloat(auction.price.replace(/,/g, ""));
+    switch (priceRange) {
+      case "upTo5k":
+        return price <= 5000;
+      case "upTo10k":
+        return price <= 10000 && price >= 5000;
+      case "upTo20k":
+        return price <= 20000 && price >= 10000;
+      case "upTo50k":
+        return price <= 50000 && price >= 20000;
+      case "upTo100k":
+        return price <= 100000 && price >= 50000;
+      case "moreThan100k":
+        return price >= 100000;
+      default:
+        return true;
+    }
+  }
+
+  let filteredArray = array;
+  switch (status) {
+    case "Active":
+      filteredArray = filteredArray.filter(isActive);
+      break;
+    case "Sold":
+      filteredArray = filteredArray.filter(isSold);
+      break;
+    case "Closed":
+      filteredArray = filteredArray.filter(isClosed);
+      break;
+    default:
+      break;
+  }
+
+  if (priceRange) {
+    filteredArray = filteredArray.filter(isPriceInRange);
   }
 
   return filteredArray;
 }
+
 
 export default filteringFunction;
